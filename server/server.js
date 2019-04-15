4194835edcb7abd3afdc6a33e016622c6945e9a9
muram <mmohamed@alertlogic.com>
2019-04-14 22:52:56 -0500

    initial api commit

---
+++ server/server.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-console */
+
+'use strict'
+
+const loopback = require('loopback')
+const boot = require('loopback-boot')
+
+const app = loopback()
+module.exports = app
+
+app.start = function appStart () {
+  // start the web server
+  return app.listen(() => {
+    app.emit('started')
+    const baseUrl = app.get('url').replace(/\/$/, '')
+    console.log('---\n')
+    console.log('Web server listening at: %s', baseUrl)
+    if (app.get('loopback-component-explorer')) {
+      const explorerPath = app.get('loopback-component-explorer').mountPath
+      console.log('Browse your REST API at %s%s', baseUrl, explorerPath)
+    }
+  })
+}
+
+const bootOptions = {
+  appRootDir: __dirname,
+  // whatever you put here is run.  But the base of `boot` is also still run
+  bootDirs: [`${__dirname}/boot/migrations`],
+}
+
+// Bootstrap the application, configure models, datasources and middleware.
+// Sub-apps like REST API are mounted via boot scripts.
+boot(app, bootOptions, (err) => {
+  if (err) throw err
+
+  // start the server if `$ node server.js`
+  if (require.main === module) app.start()
+})
